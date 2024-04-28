03c42bf8ba4da101d3c83bd3eee34c007dceaedb
Jean Carlo <jeansanto2002@gmail.com>
2024-04-28 15:25:35 -0300

    Utilizando Lazy layouts

--- app/src/main/java/br/com/saborexpress/ui/components/ProductSection.kt
+++ app/src/main/java/br/com/saborexpress/ui/components/ProductSection.kt
@@ -1,12 +1,12 @@
 package br.com.saborexpress.ui.components
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,11 +15,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.com.saborexpress.R
 import br.com.saborexpress.model.Product
 import br.com.saborexpress.sampledata.sampleProducts
 import br.com.saborexpress.ui.theme.SaborExpressTheme
-import java.math.BigDecimal
 
 @Composable
 fun ProductSection(title: String, products: List<Product>) {
@@ -30,17 +28,16 @@ fun ProductSection(title: String, products: List<Product>) {
             fontSize = 20.sp,
             fontWeight = FontWeight(400)
         )
-        Row(
+        LazyRow(
             modifier = Modifier
                 .padding(top = 8.dp)
-                .fillMaxWidth()
-                .horizontalScroll(rememberScrollState())
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
 
         ) {
-            for (p in products) {
-                ProductItem(p)
+            items(products) { product ->
+                ProductItem(product = product)
             }
         }
     }
--- app/src/main/java/br/com/saborexpress/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/saborexpress/ui/screens/HomeScreen.kt
@@ -1,30 +1,24 @@
 package br.com.saborexpress.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.saborexpress.R
 import br.com.saborexpress.model.Product
-import br.com.saborexpress.sampledata.sampleProducts
 import br.com.saborexpress.sampledata.sampleSections
 import br.com.saborexpress.ui.components.ProductSection
-import java.math.BigDecimal
 
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    Column(
+    LazyColumn(
         Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
             .padding(vertical = 16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
 
@@ -32,10 +26,12 @@ fun HomeScreen(
         for (section in sections) {
             val title = section.key
             val products = section.value
-            ProductSection(
-                title = title,
-                products = products
-            )
+            item {
+                ProductSection(
+                    title = title,
+                    products = products
+                )
+            }
         }
     }
 }
