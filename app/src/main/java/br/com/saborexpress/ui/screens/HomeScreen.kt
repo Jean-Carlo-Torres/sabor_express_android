b1eb5217766f0b3dec2cf676ff24cd32e5d019fc
Jean Carlo <jeansanto2002@gmail.com>
2024-04-29 15:09:00 -0300

    Criando o estado do campo de texto

--- app/src/main/java/br/com/saborexpress/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/saborexpress/ui/screens/HomeScreen.kt
@@ -1,10 +1,16 @@
 package br.com.saborexpress.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,21 +22,29 @@ import br.com.saborexpress.ui.components.ProductSection
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize()
-            .padding(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+    Column {
+        var text by remember { mutableStateOf("") }
+        OutlinedTextField(value = text,
+            onValueChange = { value ->
+                text = value
+            },
+            label = { })
+        LazyColumn(
+            Modifier
+                .fillMaxSize()
+                .padding(vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
 
-    ) {
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            item {
-                ProductSection(
-                    title = title,
-                    products = products
-                )
+        ) {
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductSection(
+                        title = title,
+                        products = products
+                    )
+                }
             }
         }
     }
