9a0fae65cf8cd4b1f40882307b1a203db24cafe5
Jean Carlo <jeansanto2002@gmail.com>
2024-04-29 16:19:54 -0300

    Aplicando detalhes campo de texto

--- app/src/main/java/br/com/saborexpress/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/saborexpress/ui/screens/HomeScreen.kt
@@ -3,9 +3,15 @@ package br.com.saborexpress.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,11 +30,31 @@ fun HomeScreen(
 ) {
     Column {
         var text by remember { mutableStateOf("") }
-        OutlinedTextField(value = text,
+        OutlinedTextField(
+            value = text,
             onValueChange = { value ->
                 text = value
             },
-            label = { })
+            Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp
+                )
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(100),
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Search, contentDescription = "Icone de lupa"
+                )
+            },
+            label = {
+                Text("Pesquisar")
+            },
+            placeholder = {
+                Text("O que você procura?")
+            }
+        )
         LazyColumn(
             Modifier
                 .fillMaxSize()
