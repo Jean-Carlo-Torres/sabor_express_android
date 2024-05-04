98db260c03e0f2cf759305d91c5156f71adbb5db
Jean Carlo <jeansanto2002@gmail.com>
2024-05-04 08:52:34 -0300

    Adicionando descrição expandida para o card de produto

--- app/src/main/java/br/com/saborexpress/ui/components/CardProductItem.kt
+++ app/src/main/java/br/com/saborexpress/ui/components/CardProductItem.kt
@@ -1,6 +1,7 @@
 package br.com.saborexpress.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,10 +12,15 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.Dp
@@ -31,12 +37,18 @@ import java.math.BigDecimal
 fun CardProductItem(
     product: Product,
     modifier: Modifier = Modifier,
-    elevation: Dp = 4.dp
+    expanded: Boolean = false
 ) {
+    var expandedState by remember {
+        mutableStateOf(expanded)
+    }
     Card(
         modifier
             .fillMaxWidth()
-            .heightIn(150.dp),
+            .heightIn(150.dp)
+            .clickable {
+                expandedState = !expandedState
+            },
         elevation = CardDefaults.elevatedCardElevation(),
     ) {
         Column {
@@ -62,16 +74,27 @@ fun CardProductItem(
                     text = product.price.toBrazilianCurrency()
                 )
             }
+            val textOverflow = if (expandedState) {
+                TextOverflow.Visible
+            } else {
+                TextOverflow.Ellipsis
+            }
+            val maxLines =
+                if (expandedState) Int.MAX_VALUE
+                else 2
             product.description?.let {
                 Text(
                     text = product.description,
                     Modifier
-                        .padding(16.dp)
+                        .padding(16.dp),
+                    overflow = textOverflow,
+                    maxLines = maxLines
                 )
             }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
@@ -95,10 +118,27 @@ private fun CardProductItemWithDescriptionPreview() {
         Surface {
             CardProductItem(
                 product = Product(
-                    name = "teste",
-                    price = BigDecimal("9.99"),
-                    description = LoremIpsum(20).values.first()
+                    "teste",
+                    BigDecimal("9.99"),
+                    description = LoremIpsum(10).values.first(),
                 ),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardProductItemWithDescriptionExpandedPreview() {
+    SaborExpressTheme {
+        Surface {
+            CardProductItem(
+                product = Product(
+                    "teste",
+                    BigDecimal("9.99"),
+                    description = LoremIpsum(50).values.first(),
+                ),
+                expanded = true,
             )
         }
     }
