8ae46f0dc7afd886b18a3ad476ced079690d0571
Jean Carlo <jeansanto2002@gmail.com>
2024-05-14 10:03:40 -0300

    implementando o preview e scroll no formulário

--- app/src/main/java/br/com/saborexpress/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/br/com/saborexpress/ui/activities/ProductFormActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,11 +25,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.saborexpress.R
 import br.com.saborexpress.model.Product
 import br.com.saborexpress.ui.theme.SaborExpressTheme
+import coil.compose.AsyncImage
 import java.math.BigDecimal
 
 class ProductFormActivity : ComponentActivity() {
@@ -47,9 +54,11 @@ fun ProductFormScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(start = 16.dp, end = 16.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        Spacer(modifier = Modifier)
         Text(
             text = "Criando um produto",
             modifier = Modifier.fillMaxWidth(),
@@ -57,6 +66,17 @@ fun ProductFormScreen() {
         )
         var url by remember {
             mutableStateOf("")
+        }
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url, contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
         }
         TextField(
             value = url,
@@ -131,6 +151,7 @@ fun ProductFormScreen() {
         ) {
             Text(text = "Salvar")
         }
+        Spacer(modifier = Modifier)
     }
 }
 
